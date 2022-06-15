7b6ea5d8453ed4284356e31971a24dd1f55a443f
HD_EU <82515556+HAFF-Dichtungen@users.noreply.github.com>
2022-06-15 13:12:00 +0200

    Projektdateien hinzufügen.

---
+++ Test_Console_Json/CommentsApi.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Console_Json
+{
+    public partial class CommentsApi
+    {
+        string GetReleases(string url)
+        {
+            var client = new RestClient(url);
+            var response = client.Execute(new RestRequest());
+            return response.Content;
+        }
+
+        public CommentsApi()
+        {
+            List<Root> account = JsonConvert.DeserializeObject<List<Root>>(GetReleases("https://jsonplaceholder.typicode.com/comments"));
+            Console.WriteLine("Comments \n");
+            foreach (var item in account)
+            {
+                Console.WriteLine("E-Mail: " + item.email);
+                Console.WriteLine("Message: " + item.body);
+                Console.WriteLine("\n");
+            }
+        }
+
+    }
+
+    public partial class getTestData
+    {
+        string GetReleases(string url)
+        {
+            var client = new RestClient(url);
+            var response = client.Execute(new RestRequest());
+            return response.Content;
+        }
+
+        public getTestData()
+        {
+            List<Root_> account = JsonConvert.DeserializeObject<List<Root_>>(GetReleases("http://192.168.100.61/Test/MOCK_DATA.json"));
+            Console.WriteLine("TEST \n");
+            foreach (var item in account)
+            {
+                Console.WriteLine("id: " + item.id);
+                Console.WriteLine("first_name: " + item.first_name);
+                Console.WriteLine("last_name: " + item.last_name);
+                Console.WriteLine("ip_address: " + item.ip_address);
+                Console.WriteLine("gender: " + item.gender);
+
+                Console.WriteLine("\n");
+            }
+        }
+
+    }
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Root
+    {
+        public int postId { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string body { get; set; }
+    }
+
+    public class Root_
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+        public string ip_address { get; set; }
+    }
+
+}
---
+++ Test_Console_Json/Program.cs
@@ -0,0 +1,134 @@
+﻿//using Newtonsoft.Json;
+//using RestSharp;
+
+//string GetReleases(string url)
+//{
+//    var client = new RestClient(url);
+//    var response = client.Execute(new RestRequest());
+//    return response.Content;
+//}
+//try
+//{
+//    List<Person.RootObject> account = JsonConvert.DeserializeObject<List<Person.RootObject>>(GetReleases("https://jsonplaceholder.typicode.com/users"));
+//   // List<Account> company = JsonConvert.DeserializeObject<List<Company>>(GetReleases("https://jsonplaceholder.typicode.com/users"));
+
+
+//    foreach (var item in account)
+//    {
+
+//        //Console.WriteLine(item.username + "\n" + item.name + "\n" + item.email + "\n");
+//    }
+//}
+//catch (Exception ex)
+//{
+
+//    Console.WriteLine(ex.Message);
+//}
+
+
+//public class Person
+//{
+//    public class Account
+//    {
+//        public string name { get; set; }
+//        public string username { get; set; }
+//        public string email { get; set; }
+//       // public string Company { get; set; }
+
+//        public Company Company { get; set; }
+//        //public Array company { get; set; }
+//    }
+//    public class Company
+//    {
+//        public string name { get; set; }
+
+//        //public Array company { get; set; }
+//    }
+//    public class RootObject 
+//    { 
+//    public Account Account { get; set; }
+//    }
+
+//}
+
+//using Newtonsoft.Json;
+//using RestSharp;
+
+//string GetReleases(string url)
+//{
+//    var client = new RestClient(url);
+//    var response = client.Execute(new RestRequest());
+//    return response.Content;
+//}
+
+//try
+//{
+//    List<Root> account = JsonConvert.DeserializeObject<List<Root>>(GetReleases("https://jsonplaceholder.typicode.com/users"));
+//    foreach (var item in account)
+//    {
+//        Console.WriteLine("Name: "+item.name);
+//        Console.WriteLine("Mail: " + item.email);
+//        Console.WriteLine("City: " + item.address.city);
+//        Console.WriteLine("Location: " + item.address.geo.lng +" - "+ item.address.geo.lat);
+//        Console.WriteLine("Phone: " + item.phone);
+//        Console.WriteLine("Company Name: " + item.company.name);
+//        Console.WriteLine("Nompany Phrase: " + item.company.catchPhrase);
+//        Console.WriteLine("Company Bs: " + item.company.bs);
+//        Console.WriteLine("\n");
+
+//    }
+//}
+//catch (Exception ex)
+//{
+
+//    Console.WriteLine(ex.Message);
+//}
+
+using Test_Console_Json;
+
+//CommentsApi c = new CommentsApi();
+getTestData g = new getTestData();  
+
+//Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(GetReleases("https://jsonplaceholder.typicode.com/users"));
+public class Address
+{
+    public string street { get; set; }
+    public string suite { get; set; }
+    public string city { get; set; }
+    public string zipcode { get; set; }
+    public Geo geo { get; set; }
+}
+
+public class Company
+{
+    public string name { get; set; }
+    public string catchPhrase { get; set; }
+    public string bs { get; set; }
+}
+
+public class Geo
+{
+    public string lat { get; set; }
+    public string lng { get; set; }
+}
+
+public class Root
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string username { get; set; }
+    public string email { get; set; }
+    public Address address { get; set; }
+    public string phone { get; set; }
+    public string website { get; set; }
+    public Company company { get; set; }
+}
+
+
+
+
+
+
+
+
+
